14491c18c4630a5abfb91c962d07f9f9f0073b86
KrolLew23 <aniabandola@gmail.com>
2024-05-23 17:28:50 +0200

    hooki

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ContactForm } from '../../components/ContactForm/ContactForm';
 import { ContactList } from '../../components/ContactList/ContactList';
@@ -6,88 +6,72 @@ import { Filter } from '../../components/Filter/Filter';
 
 import s from '../../components/App/App.module.css';
 
-export class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-      filter: '',
-    };
-  }
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     try {
       const json = localStorage.getItem('contacts');
       const contacts = JSON.parse(json);
 
       if (contacts) {
-        this.setState(() => ({ contacts: contacts }));
+        setContacts(contacts);
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      const json = JSON.stringify(this.state.contacts);
-      localStorage.setItem('contacts', json);
-    }
-  }
+  useEffect(() => {
+    const json = JSON.stringify(contacts);
+    localStorage.setItem('contacts', json);
+  }, [contacts]);
 
-  addContact = event => {
+  const addContact = event => {
     const loweredCase = event.name.toLowerCase().trim();
 
-    const exists = this.state.contacts.some(
+    const exists = contacts.some(
       contact => contact.name.toLowerCase().trim() === loweredCase
     );
 
     if (exists) {
       alert(`${event.name} is already in contacts!`);
     } else {
-      this.setState(({ contacts }) => ({
-        contacts: [...contacts, event],
-      }));
+      setContacts([...contacts, event]);
     }
   };
 
-  addFilter = event => {
-    this.setState({ filter: event.currentTarget.value });
+  const addFilter = event => {
+    setFilter(event.currentTarget.value);
   };
 
-  filteredContacts = () => {
-    const { filter, contacts } = this.state;
-
+  const filteredContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  deleteContact = id =>
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(contact => contact.id !== id),
-    }));
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
 
-  render() {
-    const { filter } = this.state;
-
-    return (
-      <section className={s.content}>
-        <div className={s.content__container}>
-          <ContactForm addContact={this.addContact} />
-          <ContactList
-            contacts={this.filteredContacts()}
-            deleteContact={this.deleteContact}
-          >
-            <Filter filter={filter} addFilter={this.addFilter} />
-          </ContactList>
-        </div>
-      </section>
-    );
-  }
-}
+  return (
+    <section className={s.content}>
+      <div className={s.content__container}>
+        <ContactForm addContact={addContact} />
+        <ContactList
+          contacts={filteredContacts()}
+          deleteContact={deleteContact}
+        >
+          <Filter filter={filter} addFilter={addFilter} />
+        </ContactList>
+      </div>
+    </section>
+  );
+};
